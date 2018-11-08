5b1760e9a516d2872f495d8a6d8fa088a84ca196
ooozbh <ooozbh@gmail.com>
2018-11-08 21:38:40 +0800

    function name bad usage fixed

--- knapsack.py
+++ knapsack.py
@@ -28,7 +28,6 @@ MAX_ITEM = 50
 MAX_WEIGHT = 50
 NBR_ITEMS = 20
 
-
 NGEN = 50
 MU = 50
 LAMBDA = 2
@@ -41,46 +40,51 @@ for i in range(NBR_ITEMS):
     items[i] = (random.randint(1, 10), random.uniform(0, 100))
 
 
-def evalKnapsack(individual):    
+def eval_knapsack(individual):
     weight = 0.0
     value = 0.0
     for item in individual:
         weight += items[item][0]
         value += items[item][1]
     if len(individual) > MAX_ITEM or weight > MAX_WEIGHT:
-        return 1e30, 0.0 # Ensure overweighted bags are dominated
+        return 1e30, 0.0  # Ensure overweighted bags are dominated
     return weight, value
 
-def evalKnapsackBalanced(individual):
+
+def eval_knapsack_balanced(individual):
     """
-    Variant of the original weight-value knapsack problem with added third object being minimizing weight difference between items.
+    Variant of the original weight-value knapsack problem with added
+    third object being minimizing weight difference between items.
     """
-    weight, value = evalKnapsack(individual)
+    weight, value = eval_knapsack(individual)
     balance = 0.0
-    for a,b in zip(individual, list(individual)[1:]):
-        balance += abs(items[a][0]-items[b][0])
+    for a, b in zip(individual, list(individual)[1:]):
+        balance += abs(items[a][0] - items[b][0])
     if len(individual) > MAX_ITEM or weight > MAX_WEIGHT:
-        return weight, value, 1e30 # Ensure overweighted bags are dominated
+        return weight, value, 1e30  # Ensure overweighted bags are dominated
     return weight, value, balance
 
-def cxSet(ind1, ind2):
+
+def cx_set(ind1, ind2):
     """Apply a crossover operation on input sets. The first child is the
     intersection of the two sets, the second child is the difference of the
     two sets.
     """
-    temp = set(ind1)                # Used in order to keep type
-    ind1 &= ind2                    # Intersection (inplace)
-    ind2 ^= temp                    # Symmetric Difference (inplace)
+    temp = set(ind1)  # Used in order to keep type
+    ind1 &= ind2  # Intersection (inplace)
+    ind2 ^= temp  # Symmetric Difference (inplace)
     return ind1, ind2
-    
-def mutSet(individual):
+
+
+def mut_set(individual):
     """Mutation that pops or add an element."""
     if random.random() < 0.5:
-        if len(individual) > 0:     # We cannot pop from an empty set
+        if len(individual) > 0:  # We cannot pop from an empty set
             individual.remove(random.choice(sorted(tuple(individual))))
     else:
         individual.add(random.randrange(NBR_ITEMS))
     return individual,
+
 
 def main(objectives=2, seed=64):
     random.seed(seed)
@@ -95,7 +99,6 @@ def main(objectives=2, seed=64):
         print("No evaluation function available for", objectives, "objectives.")
         sys.exit(-1)
 
-        
     creator.create("Individual", set, fitness=creator.Fitness)
 
     toolbox = base.Toolbox()
@@ -104,35 +107,35 @@ def main(objectives=2, seed=64):
     toolbox.register("attr_item", random.randrange, NBR_ITEMS)
 
     # Structure initializers
-    toolbox.register("individual", tools.initRepeat, creator.Individual, 
+    toolbox.register("individual", tools.initRepeat, creator.Individual,
                      toolbox.attr_item, IND_INIT_SIZE)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
     if objectives == 2:
-        toolbox.register("evaluate", evalKnapsack)
+        toolbox.register("evaluate", eval_knapsack)
     elif objectives == 3:
-        toolbox.register("evaluate", evalKnapsackBalanced)
+        toolbox.register("evaluate", eval_knapsack_balanced)
     else:
         print("No evaluation function available for", objectives, "objectives.")
         sys.exit(-1)
-        
 
-    toolbox.register("mate", cxSet)
-    toolbox.register("mutate", mutSet)
+    toolbox.register("mate", cx_set)
+    toolbox.register("mutate", mut_set)
     toolbox.register("select", tools.selNSGA2)
 
     pop = toolbox.population(n=MU)
     hof = tools.ParetoFront()
 
     stats = {}
+
     def lambda_factory(idx):
-        return lambda ind: ind.fitness.values[idx]                
+        return lambda ind: ind.fitness.values[idx]
 
     fitness_tags = ["Weight", "Value"]
     for tag in fitness_tags:
-        s = tools.Statistics( key=lambda_factory(
-                    fitness_tags.index(tag)
-                ))
+        s = tools.Statistics(key=lambda_factory(
+            fitness_tags.index(tag)
+        ))
         stats[tag] = s
 
     mstats = tools.MultiStatistics(**stats)
@@ -143,9 +146,10 @@ def main(objectives=2, seed=64):
 
     ea = MOEAD(pop, toolbox, MU, CXPB, MUTPB, ngen=NGEN, stats=mstats, halloffame=hof, nr=LAMBDA)
     pop = ea.execute()
-    
+
     return pop, stats, hof
-                 
+
+
 if __name__ == "__main__":
     objectives = 2
     seed = 64
@@ -154,10 +158,10 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         objectives = int(sys.argv[2])
 
-    pop,stats,hof = main(objectives)
+    pop, stats, hof = main(objectives)
 
-    pop = [str(p) +" "+ str(p.fitness.values) for p in pop]
-    hof = [str(h) +" "+ str(h.fitness.values) for h in hof]
+    pop = [str(p) + " " + str(p.fitness.values) for p in pop]
+    hof = [str(h) + " " + str(h.fitness.values) for h in hof]
     print("POP:")
     print("\n".join(pop))
 
